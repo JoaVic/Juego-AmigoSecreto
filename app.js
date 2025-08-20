6093c9c558ca1777164168418a39e467d157ba00
Renato Villalta <Renatovillalta14@gmail.com>
2025-08-20 00:04:13 -0500

    Implemente agregar amigo y mostrar lista

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let id = document.getElementById("amigo");
+    let nombre = id.value.trim();
+
+    if (nombre === ""){
+        alert("Por favor ingresa un nombre");
+        return;
+    }
+//Agregamos el array
+    amigos.push(nombre);
+    //limpiamos campo
+    id.value = "";
+    //Se muestra en la consola que si guarda nombres ingresados
+    console.log(amigos);
+
+    mostrarLista();
+}
+
+function mostrarLista(){
+    let lista = document.getElementById("listaAmigos");
+    // limpiar lista antes de volver a dibujar
+    lista.innerHTML = "";
+    //Crear un <li> por cada amigo
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+}
+}
